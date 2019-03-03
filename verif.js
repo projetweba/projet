d21f851ca853cc464da856dc58f95629d41af35f
Hanene1 <hanene.ennine@esprit.tn>
2019-03-04 00:21:26 +0100

    first commit

---
+++ verif.js
@@ -0,0 +1,13 @@
+function verifname(champ)
+{
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
